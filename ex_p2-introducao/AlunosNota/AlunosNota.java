d27794af62bf6ea609e2dccbcad89c9bacd66d90
gangorra <felipegangorra@gmail.com>
2022-05-12 12:26:50 -0300

    new

---
+++ ex_p2-introducao/AlunosNota/AlunosNota.java
@@ -0,0 +1,17 @@
+/*
+Laboratório de Programação 2 - Lab 1 (08/mai/2022)
+@author Felipe Gangorra - 121111084
+*/
+import java.util.Scanner;
+ 
+public class AlunosNota {
+   public static void main (String args[]) {
+       Scanner sc = new Scanner(System.in);
+       //entradas sendo dividida e colocando em int
+       //calculando as grandezas
+       //saida esperada
+   }
+}
+ 
+//obs: crie funções para as etapas!
+
