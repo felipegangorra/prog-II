222de42fe37a6a2e0763f63fe3d875a7073edc68
gangorra <felipegangorra@gmail.com>
2022-05-12 17:11:20 -0300

    Update

--- Exercicios/AlunosNota.java
+++ Exercicios/AlunosNota.java
@@ -1,17 +1,54 @@
 /*
-Laboratório de Programação 2 - Lab 1 (08/mai/2022)
+Laboratório de Programação 2 - Lab 1 (12/mai/2022)
 @author Felipe Gangorra - 121111084
 */
-import java.util.Scanner;
- 
-public class AlunosNota {
-   public static void main (String args[]) {
-       Scanner sc = new Scanner(System.in);
-       //entradas sendo dividida e colocando em int
-       //calculando as grandezas
-       //saida esperada
-   }
-}
- 
-//obs: crie funções para as etapas!
 
+import java.util.Scanner;
+
+public class AlunosNota {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double maior = Double.NEGATIVE_INFINITY;
+        double menor = Double.POSITIVE_INFINITY;
+        int soma = 0;
+        int abaixo = 0;
+        int acima = 0;
+        int cont = 0;
+    
+        while (true) {
+            String[] entrada = sc.nextLine().split(" ");    
+            if (entrada[0].equals("-")) {                   //parada
+                break;
+            }
+
+            double convertido = Double.parseDouble(entrada[1]);
+
+            if (convertido > maior) {
+                maior = convertido;
+            }
+            if (convertido < menor) {
+                menor = convertido;
+            }
+
+            if (convertido >= 700) {
+                acima += 1;
+            }
+            if (convertido < 700) {
+                abaixo += 1;
+            }
+
+            cont += 1;              //contador para media
+            soma += convertido;     //soma para media
+
+        }
+
+        int media = soma / cont;    //media
+
+
+        System.out.printf("maior: " + "%.0f", maior);
+        System.out.printf("\nmenor: " + "%.0f", menor);
+        System.out.println("\nmedia: " + media);
+        System.out.println("acima: " + acima);
+        System.out.println("abaixo: " + abaixo);
+    }
+}
