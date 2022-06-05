4841e181b4c992926204beeb33b56a105348fbe2
gangorra <felipegangorra@gmail.com>
2022-06-05 02:42:59 -0300

    Update

--- Importante/info.java
+++ Importante/info.java
@@ -1,6 +1,9 @@
 /*
-LINHA DE COMANDO: python3 dirlididi.py submit [questão] cwPI0PzUlWP1 [arquivo.java]
+LINHA DE COMANDO: python3 dirlididi.py submit [questão] minha-chave [arquivo.java]
 
 CANVA: https://canvas.instructure.com/
 
+DIRLIDIDI: http://dirlididi.com/client/index.html
+
+ZIPAR-COMANDO: zip -r nome.zip arquivo/pasta 
 */
